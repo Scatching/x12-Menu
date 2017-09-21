dd57c0512ec49ffe0d7fa77f073d35f23bdd80c8
scatching <scatching@mail.csuchico.edu>
2017-09-20 19:08:41 -0700

    THE MESSIAH OF CODES

--- menu.cpp
+++ menu.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 int main()
 {
+  std::string item;
   string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
@@ -16,10 +17,34 @@ int main()
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/S/Q): ";
   cin>>input;
+   if(input=='A' || input=='a')
+   {
+   cout<<"What is your item you wish to add?"<<endl;
+   cin>>item;
+   cout<<"Adding "<<item<<" to menu."<<endl;
+   list[numItems]=item;
+   numItems++;
+   if(numItems>4)
+   {
+   cout<<"You'll need a bigger list!"<<endl;
+   }}
+   if(input=='S' || input=='s')
+   {
+   for(int i=0; i<numItems; i++)
+   {
+   cout<<"Your list contains "<<list[i]<<endl;
+   }}
+
+
+
 }
 while(input!='Q' && input!='q');
-  
-
+ // if(input=='A'|| input=='a')
+ // {
+  //cout<<"What is your item you wish to add?"<<endl;
+ // cin>>item;
+ // cout<<"Adding "<<item<<" to menu."<<endl;
+ // } 
 
 
   return 0;
